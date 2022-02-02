05824fbc40f573d351043ab6397df7e288c3cbea
jihyelee <jihyelee.329@gmail.com>
2022-02-03 03:29:08 +0900

    feat : create Article component

--- src/Router/Article.js
+++ src/Router/Article.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import {getNewIds} from '../API/HNApi';
+import LookZoom from '../components/LookZoom';
+import LookSmallView from '../components/LookSmallView';
+import CheckRadio from '../components/CheckRadio'
+// import {Link, Route} from 'react-router-dom'
 
-export default function Article(){
-  return(<>
-    <h1>Article 페이지</h1>
+function Article({checked, changeChk, onZoomToggle, onToggle ,listName, setListName}){
+  const [newIds, setNewIds] = useState([]);
+  useEffect(()=>{
+    setListName("new");
+    getNewIds().then((data) => setNewIds(data));
+  },[]);
+
+  
+  return(
+    <>
+    <CheckRadio checked={checked} changeChk={changeChk} onZoomToggle={onZoomToggle} onToggle={onToggle}/>
+    {newIds.slice(0,10).map((id,index)=>(
+      onToggle ? 
+        <LookZoom id={id} key={id} index={id} listName={listName} /> :
+        <LookSmallView  id={id} key={id} index={index} listName={listName}/>
+    ))}
     </>
   )
 
 }
+export default Article;
