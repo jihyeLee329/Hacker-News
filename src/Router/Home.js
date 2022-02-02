3a6da06a00a9499d5afa66ef3b0627760eedacbc
jihyelee <jihyelee.329@gmail.com>
2022-02-02 21:44:22 +0900

    feat : top 영역 컴포넌트 추가

--- src/Router/Home.js
+++ src/Router/Home.js
@@ -2,6 +2,7 @@ import React,{useEffect, useState} from "react";
 import { TopList } from "../components/Top/TopList";
 import CheckRadio from '../components/CheckRadio'
 import LookZoom from '../components/LookZoom';
+import LookSmallView from '../components/LookSmallView';
 import { getTopStoryIds } from '../API/HNApi'
 
 //---------- 메인 컴포넌트  ------------- //
@@ -18,7 +19,11 @@ export default function Home({onDimmed, checked, changeChk ,onZoomToggle, onTogg
     <>
       <TopList onDimmed={onDimmed} topStoryIds={topStoryIds} />
       <CheckRadio checked={checked} changeChk={changeChk} onZoomToggle={onZoomToggle} onToggle={onToggle}/>
-      <LookZoom id={topStoryIds} listName={listName}/>
+      {topStoryIds.slice(0, 10).map((id, index) => (
+         onToggle ? 
+         <LookZoom id={id} key={id} index={id} listName={listName} /> :
+         <LookSmallView  id={id} key={id} index={index} listName={listName}/>
+      ))}
     </>
   );
 }
