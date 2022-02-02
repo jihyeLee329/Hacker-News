fbd943f94f52aacd194094d94e4a36b80da62553
jihyelee <jihyelee.329@gmail.com>
2022-02-02 20:15:46 +0900

    api 수정

--- src/API/HNApi.js
+++ src/API/HNApi.js
@@ -5,6 +5,8 @@ export const baseUrl = `https://hacker-news.firebaseio.com/v0/`;
 export const topStoriesUrl = `${baseUrl}topstories.json`;
 export const newStoriesUrl = `${baseUrl}newstories.json`;
 export const askStoriesUrl = `${baseUrl}askstories.json`;
+export const showStoriesUrl = `${baseUrl}showstories.json`;
+export const jobsStoriesUrl = `${baseUrl}jobstories.json`;
 export const baseItem = `${baseUrl}item/`;
 
 // home 에서 top 영역
@@ -20,7 +22,7 @@ export const getTopStoryIds = async () => {
   return result;
 };
 
-// new 영역
+//--------------- new data--------------- //
 // export const getNewStory = async (newStoryId) => {
 //   const result = await axios
 //     .get(`${baseItem + newStoryId}.json`)
@@ -35,6 +37,7 @@ export const getNewStoryIds = async () => {
 
 
 
+//--------------- Ask data--------------- //
 export const getAsk = async (getAskId) => {
   const result = await axios
     .get(`${baseItem + getAskId}.json`)
@@ -48,6 +51,33 @@ export const getAskIds = async ()=>{
 }
 
 
+
+//--------------- show data--------------- //
+export const getShow = async (getShowId) => {
+  const result = await axios
+    .get(`${baseItem + getShowId}.json`)
+    .then(({ data }) => data && selectFields(data));
+  return result;
+};
+
+export const getShowIds = async ()=>{
+  const result = await axios.get(showStoriesUrl).then(({ data }) => data);
+  return result;
+}
+
+
+//--------------- jobs data--------------- //
+export const getJobs = async (getJobsId) => {
+  const result = await axios
+    .get(`${baseItem + getJobsId}.json`)
+    .then(({ data }) => data && selectFields(data));
+  return result;
+};
+
+export const getJobsIds = async ()=>{
+  const result = await axios.get(jobsStoriesUrl).then(({ data }) => data);
+  return result;
+}
 // export const [api,setApi] = useReducer(reducer, []);
 // const reducer = (state, action ) =>{
 //   switch (action.type) {
