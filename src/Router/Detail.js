8b09e4abc2146404997ed03d8968506656440b3f
이지혜 <jihyelee.329@gmail.com>
2022-02-03 21:57:15 +0900

    feat : Detail page

---
+++ src/Router/Detail.js
@@ -0,0 +1,9 @@
+import React, { useState, useEffect } from "react";
+import Header from "../components/Header";
+
+export default function Detail() {
+  return;
+  <>
+    <Header></Header>
+  </>;
+}
