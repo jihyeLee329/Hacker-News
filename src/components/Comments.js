e20592f512e7d355b605d724012bdb731203371a
이지혜 <jihyelee.329@gmail.com>
2022-02-05 01:29:50 +0900

    feat : feat comment component

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,14 +1,69 @@
 import { useState, useEffect } from "react";
-import axios from "axios";
 import { getData } from "../API/HNApi";
+import styled from "styled-components";
+import { TimeForToday } from "./TimeForToday";
+const Comment = styled.div`
+  span {
+    font-size: 12px;
+    line-height: 16px;
+  }
+  & + & {
+    border-top: 1px solid #f0f0f6;
+  }
+  padding: 16px 0;
+  > div {
+    position: relative;
+  }
+  .user {
+    color: #ff6600;
+    padding: 0 12px;
+    border: 1px solid #ff6600;
+    border-radius: 20px;
+  }
+  .time {
+    color: #999;
+    margin-left: 8px;
+  }
+  button {
+    position: absolute;
+    right: 20px;
+  }
+  .text {
+    margin-top: 8px;
+    font-size: 16px;
+    line-height: 24px;
+    color: #505050;
+    word-break: break-word;
+  }
+`;
 export default function Comments({ kid }) {
-  const [getkid, setGetKid] = useState([]);
-  const [kidcomment, setKidcomment] = useState({});
-
+  const [kidData, setkidData] = useState([]);
+  const [toggleBtn, setToggleBtn] = useState(true);
+  function allView() {
+    setToggleBtn(!toggleBtn);
+  }
   useEffect(() => {
-    getData(kid).then((data) => data && setGetKid(data));
+    getData(kid).then((data) => data && setkidData(data));
   }, [kid]);
-  console.log(getkid);
 
-  return <div>코멘트</div>;
+  return (
+    <Comment>
+      {kidData && (
+        <>
+          <div>
+            <span className="user">{kidData.by}</span>
+            <span className="time">{TimeForToday(kidData.time)}</span>
+            <button value={toggleBtn} onClick={allView}>
+              {toggleBtn ? (
+                <img src="img/comment_toggle_view.svg" alt="펼쳐보기" />
+              ) : (
+                <img src="img/comment_toggle_sm.svg" alt="줄여보기" />
+              )}
+            </button>
+          </div>
+          {toggleBtn ? null : <div className="text">{kidData.text}</div>}
+        </>
+      )}
+    </Comment>
+  );
 }
