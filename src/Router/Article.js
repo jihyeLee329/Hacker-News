cf8400a10dc9115952479f1b1429bf1b4f7c3d75
jihyelee <jihyelee.329@gmail.com>
2022-02-07 21:05:33 +0900

    modify

--- src/Router/Article.js
+++ src/Router/Article.js
@@ -6,17 +6,17 @@ import CheckRadio from '../components/CheckRadio'
 // import {Link, Route} from 'react-router-dom'
 
 function Article({checked, changeChk, onZoomToggle, onToggle ,listName, setListName}){
-  const [newIds, setNewIds] = useState([]);
+  const [topIds, setTopIds] = useState([]);
   useEffect(()=>{
-    setListName("new");
-    getNewIds().then((data) => setNewIds(data));
+    setListName("article");
+    getNewIds().then((data) => setTopIds(data));
   },[]);
 
   
   return(
     <>
     <CheckRadio checked={checked} changeChk={changeChk} onZoomToggle={onZoomToggle} onToggle={onToggle}/>
-    {newIds.slice(0,10).map((id,index)=>(
+    {topIds.slice(0,10).map((id,index)=>(
       onToggle ? 
         <LookZoom id={id} key={id} index={id} listName={listName} /> :
         <LookSmallView  id={id} key={id} index={index} listName={listName}/>
--- src/Router/Show.js
+++ src/Router/Show.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from "react";
-import { getShowIds } from "../API/HNApi";
+import { getShowIds, getData } from "../API/HNApi";
 import LookZoom from "../components/LookZoom";
 import LookSmallView from "../components/LookSmallView";
 import CheckRadio from "../components/CheckRadio";
+import axios from "axios";
 
 // import {Link, Route} from 'react-router-dom'
 
@@ -15,10 +16,21 @@ function Show({
   setListName,
 }) {
   const [showIds, setShowIds] = useState([]);
+  const [listId, setListId] = useState({});
   useEffect(() => {
     setListName("show");
     getShowIds().then((data) => setShowIds(data));
+    return ()=>(
+      setShowIds([])
+    )
   }, []);
+
+
+  useEffect(()=>{
+    showIds.map((showId)=>(
+      getData(showId).then((data)=> data && setListId(data))));
+  },[showIds]);
+console.log(listId)
 
   return (
     <>
@@ -28,7 +40,7 @@ function Show({
         onZoomToggle={onZoomToggle}
         onToggle={onToggle}
       />
-      {showIds
+      {/* {showIds
         .slice(0, 10)
         .map((id, index) =>
           onToggle ? (
@@ -36,7 +48,7 @@ function Show({
           ) : (
             <LookSmallView id={id} key={id} index={index} listName={listName} />
           )
-        )}
+        )} */}
     </>
   );
 }
