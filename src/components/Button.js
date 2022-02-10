83a42071acd530e3158f05f3eb44d60b3b0e4e7a
이지혜 <jihyelee.329@gmail.com>
2022-02-10 18:08:16 +0900

    feat Button component

---
+++ src/components/Button.js
@@ -0,0 +1,63 @@
+import {useState, useEffect} from 'react' 
+import styled from 'styled-components'
+import arrowTop from '../img/arrow_top.png';
+
+const ButtonWrap = styled.div`
+  position:fixed; right:10px; bottom:56px; 
+`;
+const ScrollTop = styled.button`
+
+transition:all 0.4s;
+position:fixed; bottom: 56px; right:10px; 
+background: #FFFFFF;
+box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.2);
+border-radius: 12px;
+width:40px; height:40px; border:none; outline:none;
+padding:12px 14px 13px;
+opacity:0;
+&.active{opacity:1;}
+img{max-width:100%;}
+`;
+export const Button = ()=> {
+  const [ScrollY, setScrollY] = useState(0); //스크롤값 저장용
+  const [BtnStatus, setBtnStatus] = useState(false); // 버튼 상태
+  
+  //스크롤 여부에 따른 버튼 노출
+  const handleFollow = () => {
+    setScrollY(window.pageYOffset);
+    if(ScrollY > 100) {
+      // 100 이상이면 버튼이 보이게
+      setBtnStatus(true);
+    } else {
+      // 100 이하면 버튼이 사라지게
+      setBtnStatus(false);
+    }
+  }
+
+
+  const handleTop = () => {  // 클릭하면 스크롤이 위로 올라가는 함수
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    });
+    setScrollY(0);  // ScrollY 의 값을 초기화
+    setBtnStatus(false); // BtnStatus의 값을 false로 바꿈 => 버튼 숨김
+  }
+
+  useEffect(() => {
+    const watch = () => {
+      window.addEventListener('scroll', handleFollow)
+    }
+    watch();
+    return () => {
+      window.removeEventListener('scroll', handleFollow)
+    }
+  })
+
+
+  return <ButtonWrap>
+  <ScrollTop onClick={handleTop} className={BtnStatus ? "topBtn active" : "topBtn"}><img src={arrowTop} alt="위로가기버튼"/></ScrollTop>
+  
+  </ButtonWrap>
+
+}
